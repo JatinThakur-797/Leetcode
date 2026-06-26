ccc5884971b223b830e7cf95ece2bde30d37825b
JatinThakur-797 <jt6285638@gmail.com>
2026-06-26 12:16:22 +0530

    Time: 0 ms (100%), Space: 42.2 MB (39.14%) - LeetHub

--- 0461-hamming-distance/0461-hamming-distance.java
+++ 0461-hamming-distance/0461-hamming-distance.java
@@ -1,26 +1,40 @@
 class Solution {
     public int hammingDistance(int x, int y) {
         // -> 3 = 011 , 1 = 001
-        int[] xbit = new int[32];
-        int[] ybit = new int[32];
+        // int[] xbit = new int[32];
+        // int[] ybit = new int[32];
 
-        getBit(x, xbit);
-        getBit(y, ybit);
+        // getBit(x, xbit);
+        // getBit(y, ybit);
+
         int ans = 0;
-        for(int i = 0 ; i<32 ; i++){
-            if(xbit[i] != ybit[i]){
-                ans++;
-            }
+        // int i = xbit.length-1;
+        while(x != 0 || y!= 0){
+           int digit1 = x%2;
+           int digit2 = y%2;
+           if(digit1 != digit2){
+            ans++;
+           }
+
+           if(x != 0) x/= 2;
+           if(y != 0) y/=2;
+
+         
         }
+        // for(int i = 0 ; i<32 ; i++){
+        //     if(xbit[i] != ybit[i]){
+        //         ans++;
+        //     }
+        // }
         return ans;
     }
 
-    public void getBit(int x, int[] xbit){
-        int i = xbit.length-1;
-        while(x != 0){
-            xbit[i] = x % 2;
-            x /= 2;
-            i--;
-        }
-    }
+    // public void getBit(int x, int[] xbit){
+    //     int i = xbit.length-1;
+    //     while(x != 0){
+    //         xbit[i] = x % 2;
+    //         x /= 2;
+    //         i--;
+    //     }
+    // }
 }
