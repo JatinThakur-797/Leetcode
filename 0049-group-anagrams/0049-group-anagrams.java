28850cd4f36bc306165376b3a5bbfa82a22b6083
JatinThakur-797 <jt6285638@gmail.com>
2026-07-02 14:07:47 +0530

    Time: 8 ms (36.61%), Space: 50 MB (26.9%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,31 +1,17 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        char[][] ch = new char[strs.length][];
-        for (int i = 0; i < strs.length; i++) {
-            ch[i] = strs[i].toCharArray();
-        }
-        for(char[] c : ch){
-            Arrays.sort(c);
-        }
-        String[] str = new String[strs.length];
-         for (int i = 0; i < ch.length; i++) {
-            str[i] = String.valueOf(ch[i]);
-        }
-        HashMap<String, List<Integer>> map = new HashMap<>();
-        for(int i = 0 ; i<str.length ; i++){
-            String s = str[i];
-            map.computeIfAbsent(s, k -> new ArrayList<>()).add(i);
-        }
-        List<List<Integer>> values = new ArrayList<>(map.values());
-        List<List<String>> ans = new ArrayList<>();
-        for(List<Integer> v : values){
-            List<String> curr = new ArrayList<>(); 
-            for(int i : v){
-                curr.add(strs[i]);
-            }
-            ans.add(curr);
-        }
-        return ans;
+        Map<String, List<String>> map = new HashMap<>();
         
+        for (String s : strs) {
+           char[] ch = s.toCharArray();
+            Arrays.sort(ch);
+            
+            String key = String.valueOf(ch);
+            
+            map.computeIfAbsent(key, k -> new ArrayList<>()).add(s);
+        }
+        
+        return new ArrayList<>(map.values());
+    
     }
 }
