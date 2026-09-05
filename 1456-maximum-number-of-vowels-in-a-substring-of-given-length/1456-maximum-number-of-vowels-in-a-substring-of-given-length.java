9e551469f05e85fd21b8cd9d8ed4bc6f3025ce5d
JatinThakur-797 <jt6285638@gmail.com>
2026-09-05 10:11:26 +0530

    Time: 20 ms (26.9%), Space: 46.7 MB (23.83%) - LeetHub

--- 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.java
+++ 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.java
@@ -2,26 +2,49 @@ class Solution {
     public int maxVowels(String s, int k) {
         //  "leetcode", k = 3
 
+        // Approch 1
+
         // how to get vowels count in substring 
         //  Can we use prefix sum somehow:
         // [0, 1, 2, 3, 4, 5, 6, 7]
         // [0, 1, 2, 2, 2, 3, 3, 4]
-        int[] pre = new int[s.length()];
-        pre[0] = isVowel(s.charAt(0)) ? 1 : 0;
-        for(int i = 1 ; i<s.length() ; i++ ){
-            char c = s.charAt(i);
-            pre[i] = pre[i-1] + (isVowel(c) ? 1 : 0);
-        }
+        // int[] pre = new int[s.length()];
+        // pre[0] = isVowel(s.charAt(0)) ? 1 : 0;
+        // for(int i = 1 ; i<s.length() ; i++ ){
+        //     char c = s.charAt(i);
+        //     pre[i] = pre[i-1] + (isVowel(c) ? 1 : 0);
+        // }
         
-        int st = 0 , end = k-1;
-        int ans = Integer.MIN_VALUE;
-        while(end < s.length()){
+        // int st = 0 , end = k-1;
+        // int ans = Integer.MIN_VALUE;
+        // while(end < s.length()){
 
-            int vowel = pre[end] - (st == 0 ? 0 : pre[st-1]);
-            ans = Math.max(vowel , ans);
-            st++; end++;
+        //     int vowel = pre[end] - (st == 0 ? 0 : pre[st-1]);
+        //     ans = Math.max(vowel , ans);
+        //     st++; end++;
                     
+        // }
+
+
+        // Approch 2
+
+        int st = 0;
+        int ans = Integer.MIN_VALUE;
+        int count = 0;
+
+        for(int end = 0 ; end < s.length() ; end++){
+            char ch = s.charAt(end);
+            if(isVowel(ch)){
+                count++;
+            }
+
+            if(end - st + 1 == k){
+                ans = Math.max(count, ans);
+                if(isVowel(s.charAt(st)))count--;
+                st++;
+            }
         }
+
     return ans;
 
     }
